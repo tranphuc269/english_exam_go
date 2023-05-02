ce748a4b243127e310b31a2721a698b8cab8ac4b
Trần Văn Phúc <phuc260900@gmail.com>
2023-05-02 21:12:33 +0700

    add migration

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"english_exam_go/application/middleware"
 	"english_exam_go/application/routers"
+	"english_exam_go/infrastructure/data/entities"
 	"english_exam_go/infrastructure/data/repositories"
 	"english_exam_go/utils/app_logger"
 	"english_exam_go/utils/di"
@@ -133,28 +134,28 @@ func ginValidation() {
 }
 
 func serverMigration() {
-	//if err := godotenv.Load(".env"); err != nil {
-	//	panic("Can not loading .env" + err.Error())
-	//}
-	//
-	//app_logger.Init()
-	//
-	//repositories.OpenDatabase()
-	//
-	//db := repositories.GetConn()
-	//
-	//if db != nil {
-	//	err := db.Debug().AutoMigrate(
-	//		&entities.User{},
-	//		&entities.Exam{},
-	//		&entities.ExamInvite{},
-	//		&entities.ExamResult{},
-	//		&entities.QuestionAnswer{},
-	//		&entities.ExamQuestion{},
-	//	)
-	//	//db.Model(&entities.ExamQuestion{}).AddForeignKey("question_answer_id", "question_answers(id)", "CASCADE", "CASCADE")
-	//	if err != nil {
-	//		return
-	//	}
-	//}
+	if err := godotenv.Load(".env"); err != nil {
+		panic("Can not loading .env" + err.Error())
+	}
+
+	app_logger.Init()
+
+	repositories.OpenDatabase()
+
+	db := repositories.GetConn()
+	
+	if db != nil {
+		err := db.Debug().AutoMigrate(
+			&entities.User{},
+			&entities.Exam{},
+			&entities.ExamInvite{},
+			&entities.ExamResult{},
+			&entities.QuestionAnswer{},
+			&entities.ExamQuestion{},
+		)
+		//db.Model(&entities.ExamQuestion{}).AddForeignKey("question_answer_id", "question_answers(id)", "CASCADE", "CASCADE")
+		if err != nil {
+			return
+		}
+	}
 }
